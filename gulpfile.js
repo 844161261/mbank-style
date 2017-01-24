06d956a0f51efa604bb28ff940b718624fc39ad4
haoaa <290058005@qq.com>
2017-01-24 10:15:19 +0800

    首次提交

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+var gulp = require("gulp");
+var less = require('gulp-less');
+var path = require('path');
+
+var LessAutoprefix = require('less-plugin-autoprefix');
+var autoprefix = new LessAutoprefix({ browsers: ['last 2 versions'] });
+
+gulp.task('less', function () {
+  return gulp.src('./less/**/*.less')
+    .pipe(less({
+      paths: [ path.join(__dirname, 'less', 'includes') ],
+      plugins: [autoprefix];
+    }))
+    .pipe(gulp.dest('./public/css'));
+});
